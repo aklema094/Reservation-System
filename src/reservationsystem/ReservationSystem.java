b7512c65fec68a8dad51c4582cf339c646bdc672
aklema094 <aklemasultana094@gmail.com>
2025-01-02 19:18:37 +0600

    added menu

--- src/reservationsystem/ReservationSystem.java
+++ src/reservationsystem/ReservationSystem.java
@@ -1,20 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package reservationsystem;
 
-/**
- *
- * @author user
- */
+import java.util.Scanner;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class ReservationSystem {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Scanner sc = new Scanner(System.in);
+        String url = "jdbc:mysql://localhost:3306/reservation";
+        System.out.print("Enter you user name : ");
+        String userName = sc.next();
+        sc.nextLine();
+        System.out.print("Enter your password : ");
+        String password = sc.next();
+        sc.nextLine();
+
+        // loading driver
+        Class.forName("com.mysql.jdbc.Driver");
+        try {
+            Connection con = DriverManager.getConnection(url, userName, password);
+            Statement st = con.createStatement();
+
+            while (true) {
+                System.out.println("            GUEST HOUSE RESERVATION            ");
+                System.out.println("===============================================");
+                System.out.println("1. Reserve a Room");
+                System.out.println("2. View Reservations");
+                System.out.println("3. Get Room Number");
+                System.out.println("4. Update Reservation");
+                System.out.println("5. Delete Reservation");
+                System.out.println("6. Exit");
+                System.out.print("Choose an option : ");
+                int choice = sc.nextInt();
+                sc.nextLine();
+
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
-    
+
 }
